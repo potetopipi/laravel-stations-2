e42f5c411613cf4d96b398d05f2cd8652b14b5a0
poteko <poteko@example.com>
2025-09-29 21:12:56 +0900

    station3

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('practice', function() {
-    return response('practice');
-});
-
-Route::get('practice2', function() {
-                $test = 'practice2';
-    return response($test);
-});
-
-Route::get('practice3', function() {
-                $test = 'test';
-    return response($test);
-});
+use App\Http\Controllers\PracticeController;
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);

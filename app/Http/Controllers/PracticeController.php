3f479507fd2c0987b381e8349fd25a78b049d941
poteko <poteko@example.com>
2025-09-29 23:04:24 +0900

    station4

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Practice;
+
 class PracticeController extends Controller
 {
     public function sample()
@@ -12,11 +14,18 @@ class PracticeController extends Controller
     public function sample2()
     {
         $test = 'practice2';
-            return view('practice2', ['testParam' => $test]);
+        return view('practice2', ['testParam' => $test]);
     }
+
     public function sample3()
     {
         $test = 'test';
-            return view('practice3', ['testParam' => $test]);
+        return view('practice3', ['testParam' => $test]);
+    }
+
+    public function getPractice()
+    {
+        $practices = Practice::all();
+        return response()->json($practices);
     }
 }
